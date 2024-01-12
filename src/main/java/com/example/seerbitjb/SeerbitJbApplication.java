7312c7cccd89440f347a3a5fdddaddabb62972d2
oloba olalekan <olalekan.oloba@gmail.com>
2024-01-12 10:32:27 +0100

    First commit

---
+++ src/main/java/com/example/seerbitjb/SeerbitJbApplication.java
@@ -0,0 +1,13 @@
+package com.example.seerbitjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitJbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeerbitJbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seerbitjb/SeerbitJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seerbitjb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitJbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
